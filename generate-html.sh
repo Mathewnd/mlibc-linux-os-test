c036dd52462379e735b5b1f26d8962e2698943aa
Matheus <matheusuky@gmail.com>
2026-01-01 13:19:20 -0300

    proper download of managarm CI results

--- generate-html.sh
+++ generate-html.sh
@@ -18,9 +18,9 @@ rm -r ../sysroot/usr/share/os-test/out/managarm
 
 # download latest managarm results from CI
 
-#LATEST_MANAGARM_URL=$(curl -s -H "Accept: application/vnd.github+json" $(curl -s -H "Accept: application/vnd.github+json" "https://api.github.com/repos/managarm/managarm/actions/workflows/ci.yml/runs?branch=master&status=success&per_page=1" | jq -r ".workflow_runs.[0].artifacts_url") | jq -r '.artifacts[] | select(.name == "os-test-results-managarm-x86_64.tar.gz").archive_download_url')
-#echo ${LATEST_MANAGARM_URL}
-#curl --raw -o  os-test-results-managarm-x86_64.tar.gz.zip ${LATEST_MANAGARM_URL}
+LATEST_MANAGARM_URL=$(curl -s -H "Accept: application/vnd.github+json" $(curl -s -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/managarm/managarm/actions/workflows/ci.yml/runs?branch=master&status=success&per_page=1" | jq -r ".workflow_runs.[0].artifacts_url") | jq -r '.artifacts[] | select(.name == "os-test-results-managarm-x86_64.tar.gz").archive_download_url')
+echo ${LATEST_MANAGARM_URL}
+curl -L -H "Authorization: Bearer $GITHUB_TOKEN" -o os-test-results-managarm-x86_64.tar.gz.zip ${LATEST_MANAGARM_URL}
 unzip -u os-test-results-managarm-x86_64.tar.gz.zip
 tar -xf  os-test-results.tar.gz
 
